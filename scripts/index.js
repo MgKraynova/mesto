3a3d17c636ece7091e7a9d16e3028f4af5dc88c2
Marina Kraynova <kraynovamg@gmail.com>
2021-10-19 19:09:06 +0300

    feature/js: refactor

--- scripts/index.js
+++ scripts/index.js
@@ -3,13 +3,13 @@ let inputName = document.querySelector(".popup__input_content_name");
 let inputDescription = document.querySelector(
   ".popup__input_content_description"
 );
-let popupProfile = document.querySelector(".popup_type_profile");
-let editButton = document.querySelector(".profile__edit-button");
-let closeButtonProfile = document.querySelector(
+const popupProfile = document.querySelector(".popup_type_profile");
+const editButton = document.querySelector(".profile__edit-button");
+const closeButtonProfile = document.querySelector(
   ".popup__close-button_type_profile"
 );
-let nameField = document.querySelector(".profile__name-field");
-let descriptionField = document.querySelector(".profile__description-field");
+const nameField = document.querySelector(".profile__name-field");
+const descriptionField = document.querySelector(".profile__description-field");
 
 function OpenProfilePopup() {
   inputName.value = nameField.textContent;
@@ -26,7 +26,7 @@ function closeProfilePopup() {
 closeButtonProfile.addEventListener("click", closeProfilePopup);
 
 // Функция для перезаписи значений профайла при нажатии кнопки "Сохранить"
-let formElement = document.querySelector(".popup__form");
+const formElement = document.querySelector(".popup__form");
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -39,7 +39,7 @@ formElement.addEventListener("submit", formSubmitHandler);
 
 // Функция для постановки лайков на карточки
 
-function likeActive(evt) {
+function makeLikeActive(evt) {
   evt.target.classList.toggle("card__like-button_active");
 }
 
@@ -47,6 +47,36 @@ function likeActive(evt) {
 
 function deleteCard(evt) {
   evt.target.parentElement.remove();
+}
+
+// Функция клонирования карточки из template
+
+const cardTemplate = document.querySelector(".template").content;
+
+function cloneTemplateCard() {
+  return (cardElement = cardTemplate.querySelector(".card").cloneNode(true));
+}
+
+// Функция вставки карточки в блок cards
+
+const cards = document.querySelector(".cards");
+
+function insertCardToBlockCards(cardElement) {
+  cards.prepend(cardElement);
+}
+
+// Функция добавления слушателя событий карточкам
+
+function addEventListenerToCards(cardElement) {
+  cardElement
+    .querySelector(".card__like-button")
+    .addEventListener("click", makeLikeActive); // добавление слушателя события на кнопку лайк у карточек
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard); // добавление слушателя события на кнопку "удалить" у карточек
+  cardElement
+    .querySelector(".card__image")
+    .addEventListener("click", openImagePopup); // добавление слушателя события на картинку для открытия попапа с картинкой
 }
 
 // Функция для отображения 6 карточек мест при загрузке страницы
@@ -78,32 +108,21 @@ const initialCards = [
   },
 ];
 
-const cardTemplate = document.querySelector(".template").content;
-const cards = document.querySelector(".cards");
-
-function addInitialCards(item) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+function addSixInitialCards(item) {
+  cloneTemplateCard();
   cardElement.querySelector(".card__image").src = item.link;
   cardElement.querySelector(".card__image").alt = item.name;
   cardElement.querySelector(".card__title").textContent = item.name;
-  cards.prepend(cardElement);
-  cardElement
-    .querySelector(".card__like-button")
-    .addEventListener("click", likeActive); // добавление слушателя события на кнопку лайк у карточек
-  cardElement
-    .querySelector(".card__delete-button")
-    .addEventListener("click", deleteCard); // добавление слушателя события на кнопку "удалить" у карточек
-  cardElement
-    .querySelector(".card__image")
-    .addEventListener("click", openImagePopup); // добавление слушателя события на картинку для открытия попапа с картинкой
+  insertCardToBlockCards(cardElement);
+  addEventListenerToCards(cardElement);
 }
 
-initialCards.forEach(addInitialCards);
+initialCards.forEach(addSixInitialCards);
 
 // Функция для открытия и закрытия попапа добавления карточки места
-let popupPlace = document.querySelector(".popup_type_place");
-let addButton = document.querySelector(".profile__add-button");
-let closeButtonPlace = document.querySelector(
+const popupPlace = document.querySelector(".popup_type_place");
+const addButton = document.querySelector(".profile__add-button");
+const closeButtonPlace = document.querySelector(
   ".popup__close-button_type_place"
 );
 let placeNameField = document.querySelector(".popup__input_content_place-name");
@@ -125,28 +144,20 @@ closeButtonPlace.addEventListener("click", closePlacePopup);
 
 // Функция для возможности добавлять новые карточки места
 
-let newPlaceForm = document.querySelector(".popup__form_type_place");
+const newPlaceForm = document.querySelector(".popup__form_type_place");
 
-function addNewPlace(evt) {
+function addNewCardFromPopup(evt) {
   evt.preventDefault();
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cloneTemplateCard();
   cardElement.querySelector(".card__image").src = imageLinkField.value;
   cardElement.querySelector(".card__image").alt = placeNameField.value;
   cardElement.querySelector(".card__title").textContent = placeNameField.value;
-  cards.prepend(cardElement);
+  insertCardToBlockCards(cardElement);
   closePlacePopup();
-  cardElement
-    .querySelector(".card__like-button")
-    .addEventListener("click", likeActive); // добавление слушателя события на кнопку лайк у новой карточки
-  cardElement
-    .querySelector(".card__delete-button")
-    .addEventListener("click", deleteCard); // добавление слушателя события на кнопку "удалить" у новой карточки
-  cardElement
-    .querySelector(".card__image")
-    .addEventListener("click", openImagePopup); // добавление слушателя события на картинку для открытия попапа с картинкой
+  addEventListenerToCards(cardElement);
 }
 
-newPlaceForm.addEventListener("submit", addNewPlace);
+newPlaceForm.addEventListener("submit", addNewCardFromPopup);
 
 // Функция для открытия и закрытия попапа с картинкой
 
@@ -156,9 +167,9 @@ const popupImageCaption = document.querySelector(".popup__caption");
 
 function openImagePopup(evt) {
   bigImageInPopup.src = evt.target.src;
-  let cardElement = evt.target.parentElement;
-  let cardCaption = cardElement.querySelector(".card__caption");
-  let cardTitle = cardCaption.querySelector(".card__title");
+  const cardElement = evt.target.parentElement;
+  const cardCaption = cardElement.querySelector(".card__caption");
+  const cardTitle = cardCaption.querySelector(".card__title");
   popupImageCaption.textContent = cardTitle.textContent;
   bigImageInPopup.alt = cardTitle.textContent;
   popupImage.classList.add("popup_opened");
