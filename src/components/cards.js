4547458779cc457217c0aa1c456e85882e582586
Marina Kraynova <kraynovamg@gmail.com>
2021-12-06 21:10:25 +0300

    add webpack files

--- src/components/cards.js
+++ src/components/cards.js
@@ -24,3 +24,5 @@ const initialCards = [
     link: "https://images.unsplash.com/photo-1603454299098-d848f8500335?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
   },
 ];
+
+export default initialCards;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
+import './index.css';
+import initialCards from '../components/cards.js'
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
 import Section from '../components/Section.js';
